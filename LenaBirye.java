5242d37c7cfad2b798e5317f760adb232e07fb54
LNB13 <lenab13@vt.edu>
2026-08-31 16:14:20 -0400

    Change output message to include 'Lena'

--- LenaBirye.java
+++ LenaBirye.java
@@ -1,5 +1,5 @@
 public class LenaBirye {
     public static void main(String[] args) {
-        System.out.println("Your Name is on the team.");
+        System.out.println("Your Lena is on the team.");
     }
 }
