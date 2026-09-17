39645c49253f675e0ed805f737fa18bf34bdc07e
Scott042026S <scott04@vt.edu>
2026-09-17 12:04:52 -0400

    Update Calendar.java

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,14 +1,28 @@
-import java.util.Map;
+import java.util.*;
+import java.time.*;
 
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ * 
+ *  @author scottpost
+ *  @version Sep 14, 2026
+ */
 public class Calendar
 {
     // ~ Fields ................................................................
     private Habit[] habits;
     private int habitCount;
-    private Map<Habit, ArrayList<LocalDate>> completionsByhabit;
+    private Map<Habit, ArrayList<LocalDate>> completionsByHabit;
 
     // ~ Constructors ..........................................................
-
+    /**
+     * 
+     */
+public Calendar()
+{
+    }
     // ~Public Methods ........................................................
 
 }
