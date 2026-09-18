ce9ef0288736eda44efbf10c1830d4d372be3ee6
LNB13 <lenab13@vt.edu>
2026-09-18 14:44:14 -0400

    Add FrequencyRule method signatures

--- src/FrequencyRule.java
+++ src/FrequencyRule.java
@@ -1,3 +1,7 @@
+import java.time.LocalDate;
+import java.util.List;
+
+
 public abstract class FrequencyRule
 {
     //~ Fields ................................................................
@@ -6,4 +10,9 @@ public abstract class FrequencyRule
 
     //~Public  Methods ........................................................
 
+    public abstract boolean isSatisfiedForPeriod(List<LocalDate> completions, 
+        LocalDate periodStart, LocalDate periodEnd);
+    
+    public abstract String describe();
+    
 }
